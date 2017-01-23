d8cf46aa8d6b277e4eaa627c67f784fbabdece5c
aayush-bhardwaj <aayush.bhj@gmail.com>
2017-01-23 17:45:21 +0530

    Basic React skeleton

---
+++ README.md
@@ -0,0 +1,5 @@
+## DOUBTS 
+
+1: What is <!DOCTYPE> in HTML.
+2: What is <meta> tags in <head>
+3: 
---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config'
+import webpackHotMiddleware from 'webpack-hot-middleware';
+
+let app = express();
+
+const compiler = webpack(webpackConfig);
+app.use(webpackMiddleware(compiler,{
+	hot : true,
+	noInfo : true,
+	publicPath : webpackConfig.output.publicpath,
+}));
+app.use(webpackHotMiddleware(compiler));
+
+app.get('/*',(req,res) => {
+	res.sendFile(path.join(__dirname , 'index.html'));
+});
+
+app.listen(3000, () => {
+	console.log("running on port 3000");
+})
